0848d0a7d4dcd35e40740f99d311305526679560
Dorota Palmąka <dorotapalmaka@wp.pl>
2022-10-24 15:19:30 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+let body = document.querySelector(".body");
+let dark = document.querySelector(".dark");
+let nextColour = document.querySelector(".js-nextColour");
+
+
+changeBackgroundButton.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+
+    if (body.classList.contains("dark")) {
+        nextColour.innerText = "jasny";
+    } else {
+        nextColour.innerText = "ciemny";
+    }
+});
+
+let buttonTwo = document.querySelector(".buttonTwo");
+let header = document.querySelector(".header");
+
+buttonTwo.addEventListener("click", () => {
+    header.remove();
+});
