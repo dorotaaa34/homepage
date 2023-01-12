92c8000ca4e6df3f04ba1c3862454f07c2757679
Dorota Palmąka <dorotapalmaka@wp.pl>
2023-01-12 10:50:37 +0100

    script.js formatted

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 {
-    const welcome = () => {
-        console.log ("Hello");
+  const welcome = () => {
+    console.log("Hello");
+  };
+
+  const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const nextColour = document.querySelector(".js-nextColour");
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+      nextColour.innerText = "jasny";
+    } else {
+      nextColour.innerText = "ciemny";
     }
-    
-    const toggleBackground = () => {
-        const body = document.querySelector(".body");
-        const nextColour = document.querySelector(".js-nextColour");
-        body.classList.toggle("dark");
-    
-        if (body.classList.contains("dark")) {
-            nextColour.innerText = "jasny";
-        } else {
-            nextColour.innerText = "ciemny";
-        }
-    }
-    
-    const init = () => {
-    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+  };
+
+  const init = () => {
+    const changeBackgroundButton = document.querySelector(
+      ".js-changeBackgroundButton"
+    );
     changeBackgroundButton.addEventListener("click", toggleBackground);
 
     welcome();
-    }
+  };
 
-    init ();
+  init();
 
-   const remove = () => {
+  const remove = () => {
     const dark = document.querySelector(".dark");
     const buttonTwo = document.querySelector(".buttonTwo");
     const header = document.querySelector(".header");
-    
-    buttonTwo.addEventListener("click", () => {
-        header.remove();
-    });
-   }
 
-   remove ();
-   
+    buttonTwo.addEventListener("click", () => {
+      header.remove();
+    });
+  };
+
+  remove();
 }
